a542be3c860b9eec846d58eedc4b3ed25bdd994e
Sebastian Elstadt <elstadt.sebastian@gmail.com>
2023-09-09 13:53:12 +0200

    app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.elstadt.apps.bluetoothmaster',
-  appName: 'BluetoothMaster-App',
+  appName: 'Bluetooth Master',
   webDir: 'www',
   server: {
     androidScheme: 'https'
