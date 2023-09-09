9b248551036f30e85d8b60f82c91340573c7173d
Sebastian Elstadt <elstadt.sebastian@gmail.com>
2023-09-09 13:52:02 +0200

    config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.elstadt.apps.bluetoothmaster',
   appName: 'BluetoothMaster-App',
   webDir: 'www',
   server: {
